0ff06b13bcc8acf1350e77778bc52d70742f4c8f
makura <makura6706@gmail.com>
2016-11-01 18:21:55 +0900

    修正した

--- C++/ITP1/ITP1_6_D.cpp
+++ C++/ITP1/ITP1_6_D.cpp
@@ -4,16 +4,17 @@ using namespace std;
 int main(){
   int n, m;
   cin >> n >> m;
-  int a[n][m] = {0}, b[m] = {0};
+  int a[n * m] = {0}, b[m] = {0};
   int c[n] = {0};
+
   for(int i = 0; i < n; i++)
     for(int j = 0; j < m; j++)
-      cin >> a[i][j];
+      cin >> a[i * m + j];
   for(int i = 0; i < m; i++)
     cin >> b[i];
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
-      c[i] += a[i][j] * b[j];
+      c[i] += a[i * m + j] * b[j];
     }
   }
   for(int i = 0; i < n; i++)
