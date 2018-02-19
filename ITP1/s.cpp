50c4ec22939f119aec699428b3744576dcb1ac11
makura <makura6706@gmail.com>
2018-02-19 18:51:55 +0900

    organize the repository

--- ITP1/s.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main() {
-	double a = 4;
-	cout << pow(a, (1 / 2)) << endl;
-}
--- Vol0/s.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main() {
-	double x = 3.14;
-	cout << roundf(x) << endl;
-}
